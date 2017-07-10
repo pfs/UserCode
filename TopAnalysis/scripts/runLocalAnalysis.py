a14e0a7948e2cc6725dc8a62a0cba82d5e0ba2c3
Vichayanun Wachirapusitanand <vichayanun@hotmail.com>
2017-07-10 15:12:49 +0200

    Add logging from condor

--- TopAnalysis/scripts/runLocalAnalysis.py
+++ TopAnalysis/scripts/runLocalAnalysis.py
@@ -189,6 +189,7 @@ def main():
             condor.write('executable = {0}/$(cfgFile).sh\n'.format(FarmDirectory))
             condor.write('output     = {0}/output_$(cfgFile).out\n'.format(FarmDirectory))
             condor.write('error      = {0}/output_$(cfgFile).err\n'.format(FarmDirectory))
+            condor.write('log        = {0}/output_$(cfgFile).log\n'.format(FarmDirectory))
 
             jobNb=0
             for method,inF,outF,channel,charge,flav,runSysts,systVar,era,tag,debug in task_list:

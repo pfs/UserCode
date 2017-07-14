2a9ed2fff8dd73de7ea95df8288e83e0458291ae
Vichayanun Wachirapusitanand <vichayanun@hotmail.com>
2017-07-14 15:39:11 +0200

    Fixed TMVA training code location

--- TopAnalysis/test/summer2017/condor_TMVATrain.sh
+++ TopAnalysis/test/summer2017/condor_TMVATrain.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /afs/cern.ch/user/v/vwachira/CMSSW_8_0_28
 eval `scram r -sh`
-cd src/TopLJets2015/TopAnalysis/test/summer2017
+cd src/TopLJets2015/TopAnalysis/test/summer2017/FourTops
 root -l -q FourTopsTMVA_606.cpp

f1cf2677138754586d944072376a92e970908d09
Vichayanun Wachirapusitanand <vichayanun@hotmail.com>
2017-07-21 15:21:54 +0200

    Fixed bug

--- TopAnalysis/test/summer2017/FourTops/CompareSystVar.cpp
+++ TopAnalysis/test/summer2017/FourTops/CompareSystVar.cpp
@@ -22,8 +22,6 @@ void CompareSystVar()
     TH1F *hist;
     TLegend *legend;
 
-    gStyle->SetStats(false);
-
     for (short i = 0; i < 4; i++)
     {
         canvas = new TCanvas(histograms[i].c_str(),histograms[i].c_str(),800,600);
@@ -35,6 +33,7 @@ void CompareSystVar()
             hist->SetMarkerStyle(markerStyle[j]);
             hist->SetLineColor(markerColour[j]);
             hist->SetMarkerColor(markerColour[j]);
+            hist->SetStats(false);
             hist->Draw("SAME");
             legend->AddEntry(hist, fileName[j].c_str(), "lep");
         }

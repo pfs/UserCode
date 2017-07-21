425d94d11e4922286d08909f5169a531c3437658
Vichayanun Wachirapusitanand <vichayanun@hotmail.com>
2017-07-21 14:11:27 +0200

    Added script to compare systematic variations of four-tops

---
+++ TopAnalysis/test/summer2017/CompareSystVar.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <TFile.h>
+#include <string.h>
+
+void CompareSystVar()
+{
+    TFile *FourTops_nosys = new TFile("MC13TeV_TTTT.root");
+    TFile *FourTops_fsrdn = new TFile("MC13TeV_TTTT_fsrdn.root");
+    TFile *FourTops_fsrup = new TFile("MC13TeV_TTTT_fsrup.root");
+    TFile *FourTops_isrdn = new TFile("MC13TeV_TTTT_isrdn.root");
+    TFile *FourTops_isrup = new TFile("MC13TeV_TTTT_isrup.root");
+
+    TFile *FourTops_addr[] = {FourTops_nosys, FourTops_fsrdn, FourTops_isrdn, FourTops_fsrup, FourTops_isrup};
+
+    std::string histograms[] = {"nbjets", "njets", "nleptons", "nvtx"};
+
+    TFile *out = new TFile("Compare_TTTT.root");
+    TCanvas *canvas;
+    TH1F *hist;
+
+    for (short i = 0; i < 4; i++)
+    {
+        canvas = new TCanvas(histograms[i],histograms[i],800,600);
+        canvas->cd();
+        for (short j = 0; j < 5; j++)
+        {
+            hist = (TH1F*) FourTops_addr[j]->Get(histograms[i]);
+            hist->Draw("SAME");
+        }
+        out->cd();
+        canvas->Write();
+    }
+}

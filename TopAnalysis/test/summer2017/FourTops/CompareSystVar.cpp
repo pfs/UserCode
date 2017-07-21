32be5ee23d72aa8a25f75f292a5f7ece409b2331
Vichayanun Wachirapusitanand <vichayanun@hotmail.com>
2017-07-21 14:48:58 +0200

    Added colours to comparison plot

--- TopAnalysis/test/summer2017/FourTops/CompareSystVar.cpp
+++ TopAnalysis/test/summer2017/FourTops/CompareSystVar.cpp
@@ -12,21 +12,31 @@ void CompareSystVar()
 
     TFile *FourTops_addr[] = {FourTops_nosys, FourTops_fsrdn, FourTops_isrdn, FourTops_fsrup, FourTops_isrup};
 
+    std::string fileName[] = {"FourTops_nosys", "FourTops_fsrdn", "FourTops_isrdn", "FourTops_fsrup", "FourTops_isrup"};
     std::string histograms[] = {"nbjets", "njets", "nleptons", "nvtx"};
+    int markerStyle[] = {24, 20, 21, 22, 23};
+    int markerColour[] = {1, 2, 3, 4, 5};
 
     TFile *out = new TFile("Compare_TTTT.root","RECREATE");
     TCanvas *canvas;
     TH1F *hist;
+    TLegend *legend;
 
     for (short i = 0; i < 4; i++)
     {
         canvas = new TCanvas(histograms[i].c_str(),histograms[i].c_str(),800,600);
         canvas->cd();
+        legend = new TLegend(0.5, 0.7, 0.9, 0.9);
         for (short j = 0; j < 5; j++)
         {
             hist = (TH1F*) FourTops_addr[j]->Get(histograms[i].c_str());
+            hist->SetMarkerStyle(markerStyle[j]);
+            hist->SetLineColor(markerColour[j]);
+            hist->SetMarkerColor(markerColour[j]);
             hist->Draw("SAME");
+            legend->AddEntry(hist, fileName[j], "lep");
         }
+        legend->Draw();
         out->cd();
         canvas->Write();
     }

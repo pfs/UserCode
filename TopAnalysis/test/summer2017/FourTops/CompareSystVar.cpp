2ce958253e0b4b9184e50402052fcba5d20a926b
Vichayanun Wachirapusitanand <vichayanun@hotmail.com>
2017-07-23 09:17:26 +0200

    Added BDT and MLP_ANN plots

--- TopAnalysis/test/summer2017/FourTops/CompareSystVar.cpp
+++ TopAnalysis/test/summer2017/FourTops/CompareSystVar.cpp
@@ -13,7 +13,7 @@ void CompareSystVar()
     TFile *FourTops_addr[] = {FourTops_nosys, FourTops_fsrdn, FourTops_isrdn, FourTops_fsrup, FourTops_isrup};
 
     std::string fileName[] = {"FourTops_nosys", "FourTops_fsrdn", "FourTops_isrdn", "FourTops_fsrup", "FourTops_isrup"};
-    std::string histograms[] = {"nbjets", "njets", "nleptons", "nvtx"};
+    std::string histograms[] = {"nbjets", "njets", "nleptons", "nvtx", "bdt", "mlp_ann"};
     int markerStyle[] = {24, 20, 21, 22, 23};
     int markerColour[] = {1, 2, 3, 4, 5};
 
@@ -22,7 +22,7 @@ void CompareSystVar()
     TH1F *hist;
     TLegend *legend;
 
-    for (short i = 0; i < 4; i++)
+    for (short i = 0; i < 6; i++)
     {
         canvas = new TCanvas(histograms[i].c_str(),histograms[i].c_str(),800,600);
         canvas->cd();

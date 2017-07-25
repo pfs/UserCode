3064686fd8d6e0e5682064a1ae7767d1c78fa23f
Vichayanun Wachirapusitanand <vichayanun@hotmail.com>
2017-07-25 17:19:07 +0200

    Added least square fit for signal factor calculation

--- TopAnalysis/test/summer2017/FourTops/FourTopsXSect.cpp
+++ TopAnalysis/test/summer2017/FourTops/FourTopsXSect.cpp
@@ -6,100 +6,6 @@ void FourTopsXSect()
 {
     string dir("/afs/cern.ch/user/v/vwachira/CMSSW_8_0_28/src/TopLJets2015/TopAnalysis/test/summer2017/FourTops/");
 
-    /*
-    string sigFileName("MC13TeV_TTTT.root");
-    std::vector<string> bgFileName =
-    {
-        "MC13TeV_TTJets.root",
-        "MC13TeV_SingleTbar_tW.root",
-        "MC13TeV_SingleT_tW.root",
-        "MC13TeV_SingleTbar_t.root",
-        "MC13TeV_SingleT_t.root",
-        "MC13TeV_DY50toInf_mlm.root",
-        "MC13TeV_DY10to50.root",
-        "MC13TeV_ZZTo2L2Nu.root",
-        "MC13TeV_ZZTo2L2Q.root",
-        "MC13TeV_WWToLNuQQ.root",
-        "MC13TeV_WWTo2L2Nu.root",
-        "MC13TeV_WZTo3LNu.root",
-        //"MC13TeV_TTWToQQ.root",
-        "MC13TeV_TTZToQQ.root",
-        "MC13TeV_TTZToLLNuNu.root",
-        "MC13TeV_tZq.root",
-        "MC13TeV_ZZ4L.root",
-        "MC13TeV_GGHZZ4L.root",
-        "MC13TeV_VHtoNonbb.root",
-        "MC13TeV_WWW.root",
-        "MC13TeV_WWZ.root",
-        "MC13TeV_ZZZ.root",
-        "MC13TeV_TTWW.root",
-        "MC13TeV_TTTW.root",
-        "MC13TeV_TTWZ.root",
-        "MC13TeV_TTZZ.root",
-        "MC13TeV_TTWH.root",
-        "MC13TeV_TTZH.root",
-        "MC13TeV_TTHH.root",
-        "MC13TeV_TTTJ.root"
-    };
-
-    std::vector<string> dataFileName = 
-    {
-        "Data13TeV_DoubleEG_2016B.root",
-        "Data13TeV_DoubleEG_2016C.root",
-        "Data13TeV_DoubleEG_2016D.root",
-        "Data13TeV_DoubleEG_2016E.root",
-        "Data13TeV_DoubleEG_2016F.root",
-        "Data13TeV_DoubleEG_2016G.root",
-        "Data13TeV_DoubleEG_2016Hv2.root",
-        "Data13TeV_DoubleEG_2016Hv3.root",
-        "Data13TeV_DoubleMuon_2016B.root",
-        "Data13TeV_DoubleMuon_2016C.root",
-        "Data13TeV_DoubleMuon_2016D.root",
-        "Data13TeV_DoubleMuon_2016E.root",
-        "Data13TeV_DoubleMuon_2016F.root",
-        "Data13TeV_DoubleMuon_2016G.root",
-        "Data13TeV_DoubleMuon_2016Hv2.root",
-        "Data13TeV_DoubleMuon_2016Hv3.root",
-        "Data13TeV_MuonEG_2016B.root",
-        "Data13TeV_MuonEG_2016C.root",
-        "Data13TeV_MuonEG_2016D.root",
-        "Data13TeV_MuonEG_2016E.root",
-        "Data13TeV_MuonEG_2016F.root",
-        "Data13TeV_MuonEG_2016G.root",
-        "Data13TeV_MuonEG_2016Hv2.root",
-        "Data13TeV_MuonEG_2016Hv3.root",
-        "Data13TeV_SingleElectron_2016B.root",
-        "Data13TeV_SingleElectron_2016C.root",
-        "Data13TeV_SingleElectron_2016D.root",
-        "Data13TeV_SingleElectron_2016E.root",
-        "Data13TeV_SingleElectron_2016F.root",
-        "Data13TeV_SingleElectron_2016G.root",
-        "Data13TeV_SingleElectron_2016Hv2.root",
-        "Data13TeV_SingleElectron_2016Hv3.root",
-        "Data13TeV_SingleMuon_2016B.root",
-        "Data13TeV_SingleMuon_2016C.root",
-        "Data13TeV_SingleMuon_2016D.root",
-        "Data13TeV_SingleMuon_2016E.root",
-        "Data13TeV_SingleMuon_2016F.root",
-        "Data13TeV_SingleMuon_2016G.root",
-        "Data13TeV_SingleMuon_2016Hv2.root",
-        "Data13TeV_SingleMuon_2016Hv3.root"
-    };
-
-    TFile *sigFile = new TFile((dir+sigFileName).c_str());
-    std::vector<TFile*> bgFilePtr;
-    std::vector<TFile*> dataFilePtr;
-
-    for (int i=0;i<bgFileName.size();i++) 
-        bgFilePtr.push_back(new TFile((dir+bgFileName[i]).c_str()));
-
-    for (int i=0;i<dataFileName.size();i++)
-        dataFilePtr.push_back(new TFile((dir+dataFileName[i]).c_str()));
-
-    printf("bgFilePtr.size() = %d\n",bgFilePtr.size());
-    printf("dataFilePtr.size() = %d\n",dataFilePtr.size());
-    */
-
     TFile *plotterFile = new TFile("plots/plotter.root");
     string sigProcess = "t#bar{t}t#bar{t} x 100";
     std::vector<string> bgProcess =
@@ -120,7 +26,7 @@ void FourTopsXSect()
 
     for (int i=0;i<histograms.size();i++)
     {
-        printf("Now working on %s\n",histograms[i].c_str());
+        //printf("Now working on %s\n",histograms[i].c_str());
         histCache = (TH1F*) plotterFile->Get((histograms[i]+"/"+histograms[i]+"_"+sigProcess).c_str());
 
         int numBins = histCache->GetNbinsX();
@@ -128,6 +34,7 @@ void FourTopsXSect()
         double y_sig [numBins];
         double y_bg  [numBins];
         double y_data[numBins];
+        double y_data_nobg[numBins];
 
         for (int j=0;j<numBins;j++)
         {
@@ -135,20 +42,20 @@ void FourTopsXSect()
             y_bg[j] = 0.;
             y_data[j] = 0.;
         }
-        printf("y_sig filled\n");
+        //printf("y_sig filled\n");
 
         for (int k=0;k<bgProcess.size();k++) 
         {
             histCache = (TH1F*) plotterFile->Get((histograms[i]+"/"+histograms[i]+"_"+bgProcess[k]).c_str());
-            printf("histograms[i] obtained\n");
+            //printf("histograms[i] obtained\n");
             for (int j=0;j<numBins;j++) y_bg[j] += histCache->GetBinContent(j+1);
         }
-        printf("y_bg filled\n");
+        //printf("y_bg filled\n");
 
         histCache = (TH1F*) plotterFile->Get((histograms[i]+"/"+histograms[i]).c_str());
-        printf("histograms[i] obtained\n");
+        //printf("histograms[i] obtained\n");
         for (int j=0;j<numBins;j++) y_data[j] += histCache->GetBinContent(j+1);
-        printf("y_data filled\n");
+        //printf("y_data filled\n");
 
         printf("%s",histograms[i].c_str());
         printf("\n---------------------------------------\n");
@@ -158,5 +65,26 @@ void FourTopsXSect()
         for (int i=0;i<numBins;i++) printf("%lf ",y_bg[i]);
         printf("\nDATA\n");
         for (int i=0;i<numBins;i++) printf("%lf ",y_data[i]);
+
+        for (int j=0;j<numBins;j++) 
+        {
+            y_data_nobg[j] = y_data[j]-y_bg[j];
+            if (y_data_nobg[j] < 0) y_data_nobg[j] = 0;
+        }
+
+        double sig_sq = 0.;
+        double sig_data = 0.;
+
+        for (int j=0;j<numBins;j++) 
+        {
+            sig_sq += y_sig[j]*y_sig[j];
+            sig_data += y_sig[j]*y_data[j];
+        }
+
+        factor[i] = sig_data/sig_sq;
+
     }
+
+    printf("\n==================RESULTS=====================\n");
+    for (int i=0;i<histograms.size();i++) printf("%s\t%lf\n",histograms[i],factor[i]);
 }

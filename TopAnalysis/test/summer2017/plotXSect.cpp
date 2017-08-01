fc321e4a6497c3e6274d6d02624c2c7f4d86dfbd
Vichayanun Wachirapusitanand <vichayanun@hotmail.com>
2017-08-01 14:18:43 +0200

    Added another dataset

--- TopAnalysis/test/summer2017/plotXSect.cpp
+++ TopAnalysis/test/summer2017/plotXSect.cpp
@@ -3,13 +3,16 @@
 
 void plotXSect()
 {
-    TFile *file = new TFile("datacard_4tops/fourtops_nn_Asymptotic.root");
-    TTree *tree = (TTree*) file->Get("limit");
+    TFile *fileIndex[2];
+    fileIndex[0] = new TFile("datacard_4tops/fourtops_nn_Asymptotic.root");
+    fileIndex[1] = new TFile("datacard_4tops/fourtops_bdt_Asymptotic.root");
+    //TFile *file = new TFile("datacard_4tops/fourtops_nn_Asymptotic.root");
+    TTree *tree;
 
     TCanvas *canvas = new TCanvas("canvas","Upper limits for four tops",800,600);
     canvas->Draw();
     canvas->cd();
-    TH1F *histFrame = gPad->DrawFrame(0,0,50,4);
+    TH1F *histFrame = gPad->DrawFrame(0,0,80,4);
     histFrame->SetXTitle("95% CL Limit on #mu = #sigma_{obs} / #sigma_{SM}");
     histFrame->GetYaxis()->SetTickSize(0);
     histFrame->GetYaxis()->SetLabelSize(0);
@@ -21,41 +24,45 @@ void plotXSect()
     double limitobs;
     double *limitAddr[] = {&limit2sigdown, &limit1sigdown, &limit0, &limit1sigup, &limit2sigup, &limitobs};
 
-    tree->SetBranchAddress("limit",&limitVal);
-    for (int i=0;i<tree->GetEntries();i++)
+    for (int set = 0; set < 2; set++)
     {
-        tree->GetEntry(i);
-        *limitAddr[i] = limitVal;
-        //printf("limitVal = %lf\n",limitVal);
+        tree = file[set]->Get("limit");
+        tree->SetBranchAddress("limit",&limitVal);
+        for (int i=0;i<tree->GetEntries();i++)
+        {
+            tree->GetEntry(i);
+            *limitAddr[i] = limitVal;
+            //printf("limitVal = %lf\n",limitVal);
+        }
+
+        printf("limit2sigdown = %lf\n",limit2sigdown);
+        printf("limit2sigup   = %lf\n",limit2sigup);
+        printf("limit1sigdown = %lf\n",limit1sigdown);
+        printf("limit1sigup   = %lf\n",limit1sigup);
+        printf("limit0        = %lf\n",limit0);
+        printf("limitobs      = %lf\n",limitobs);
+
+        canvas->cd();
+        box2sig = new TBox(limit2sigdown, set, limit2sigup, set+1);
+        box2sig->SetFillColor(17);
+        box2sig->Draw();
+        box1sig = new TBox(limit1sigdown, set, limit1sigup, set+1);
+        box1sig->SetFillColor(15);
+        box1sig->Draw();
+
+        lineexp = new TLine(limit0, set, limit0, set+1);
+        lineexp->Draw();
+        lineexp->SetLineWidth((Width_t)2);
+        lineobs = new TLine(limitobs, set, limitobs, set+1);
+        lineobs->SetLineStyle(7);
+        lineobs->SetLineWidth((Width_t)2);
+        lineobs->Draw();
+
+        TMarker *marker = new TMarker(limit0, 0.5, 31);
+        marker->Draw();
     }
 
-    printf("limit2sigdown = %lf\n",limit2sigdown);
-    printf("limit2sigup   = %lf\n",limit2sigup);
-    printf("limit1sigdown = %lf\n",limit1sigdown);
-    printf("limit1sigup   = %lf\n",limit1sigup);
-    printf("limit0        = %lf\n",limit0);
-    printf("limitobs      = %lf\n",limitobs);
-
-    canvas->cd();
-    box2sig = new TBox(limit2sigdown, 0, limit2sigup, 1);
-    box2sig->SetFillColor(17);
-    box2sig->Draw();
-    box1sig = new TBox(limit1sigdown, 0, limit1sigup, 1);
-    box1sig->SetFillColor(15);
-    box1sig->Draw();
-
-    lineexp = new TLine(limit0,0,limit0,1);
-    lineexp->Draw();
-    lineexp->SetLineWidth((Width_t)2);
-    lineobs = new TLine(limitobs,0,limitobs,1);
-    lineobs->SetLineStyle(7);
-    lineobs->SetLineWidth((Width_t)2);
-    lineobs->Draw();
-
-    TMarker *marker = new TMarker(limit0,0.5,31);
-    marker->Draw();
-
-    TLine *lineSM = new TLine(1,0,1,4);
+    TLine *lineSM = new TLine(1, 0, 1, 4);
     lineSM->SetLineColor(kRed);
     lineSM->SetLineWidth((Width_t)5);
     lineSM->Draw();

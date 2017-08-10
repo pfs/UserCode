d9b2b22a6c36d9249b51e4b446920c563337d635
Vichayanun Wachirapusitanand <vichayanun@hotmail.com>
2017-08-10 17:08:29 +0200

    Added SS Dilepton in the plot

--- TopAnalysis/test/summer2017/plotXSect.cpp
+++ TopAnalysis/test/summer2017/plotXSect.cpp
@@ -3,7 +3,7 @@
 
 void plotXSect()
 {
-    const int numberOfDatasets = 5;
+    const int numberOfDatasets = 6;
     TFile *fileIndex[numberOfDatasets];
     fileIndex[0] = new TFile("datacard_4tops/fourtops_nn_Asymptotic.root");
     fileIndex[1] = new TFile("datacard_4tops/fourtops_bdt_Asymptotic.root");
@@ -11,12 +11,12 @@ void plotXSect()
     TTree *tree;
     
     // Dataset name here
-    string fileName[] = {"#splitline{#bf{Four lepton NN}}{#scale[0.7]{This analysis}}", "#splitline{#bf{Four lepton BDT}}{#scale[0.7]{This analysis}}", "#splitline{#bf{Dilepton}}{#scale[0.7]{TOP-16-016}}",  "#splitline{#bf{Single lepton}}{#scale[0.7]{TOP-16-016}}", "#splitline{#bf{Combined}}{#scale[0.7]{TOP-16-016}}"};
+    string fileName[] = {"#splitline{#bf{Four lepton NN}}{#scale[0.7]{This analysis}}", "#splitline{#bf{Four lepton BDT}}{#scale[0.7]{This analysis}}", "#splitline{#bf{SS Dilepton}}{#scale[0.7]{EPJC 76 (2016) 439}}", "#splitline{#bf{OS Dilepton}}{#scale[0.7]{TOP-16-016}}",  "#splitline{#bf{Single lepton}}{#scale[0.7]{TOP-16-016}}", "#splitline{#bf{Combined}}{#scale[0.7]{TOP-16-016}}"};
 
     TCanvas *canvas = new TCanvas("canvas","Upper limits for four tops",800,600);
     canvas->Draw();
     canvas->cd();
-    TH1F *histFrame = gPad->DrawFrame(0,-0.15,100,7);
+    TH1F *histFrame = gPad->DrawFrame(0,-0.15,100,8);
     histFrame->SetXTitle("95% CL Limit on #mu = #sigma_{obs} / #sigma_{SM}");
     histFrame->GetYaxis()->SetTickSize(0);
     histFrame->GetYaxis()->SetLabelSize(0);
@@ -37,19 +37,22 @@ void plotXSect()
     double limitYPointCoord[numberOfDatasets], limitYPointCoordErr[numberOfDatasets];
 
     // Hardcoding values for plots
-    limitObs[2] = 14.5; // Dilepton
-    limitObs[3] = 17.2; // Single lepton
-    limitObs[4] = 10.2; // Combined
+    limitObs[2] = 12.9; // SS Dilepton
+    limitObs[3] = 14.5; // OS Dilepton
+    limitObs[4] = 17.2; // Single lepton
+    limitObs[5] = 10.2; // Combined
     limitObsError[2] = 0.;
     limitObsError[3] = 0.;
     limitObsError[4] = 0.;
-    limit0[2] = 24.7;
-    limit0[3] = 16.4;
-    limit0[4] = 12.8;
+    limitObsError[5] = 0.;
+    limit0[2] = 11.0;
+    limit0[3] = 24.7;
+    limit0[4] = 16.4;
+    limit0[5] = 12.8;
 
-    for (int y = 2; y < 5; y++)
+    for (int y = 2; y < 6; y++)
     {
-        limitYPointCoord[y] = 4 - y + 0.5;
+        limitYPointCoord[y] = 5 - y + 0.5;
         limitYPointCoordErr[y] = 0.5;
     }
 
@@ -70,7 +73,7 @@ void plotXSect()
         limitObs[s] = limitObsCache;
         tree->GetEntry(5);
         limitObsError[s] = limitErrVal;
-        limitYPointCoord[s] = (4.5-s) + 0.5;
+        limitYPointCoord[s] = (5.5-s) + 0.5;
         limitYPointCoordErr[s] = 0.5;
 
         /*
@@ -84,36 +87,41 @@ void plotXSect()
 
         // Draw 2-sig box first ...
         canvas->cd();
-        box2sig = new TBox(limit2sigdown, (4.5-s), limit2sigup, (4.5-s)+1);
+        box2sig = new TBox(limit2sigdown, (5.5-s), limit2sigup, (5.5-s)+1);
         box2sig->SetFillColor(17);
         box2sig->Draw();
         // ... and 1-sig box on top of it.
-        box1sig = new TBox(limit1sigdown, (4.5-s), limit1sigup, (4.5-s)+1);
+        box1sig = new TBox(limit1sigdown, (5.5-s), limit1sigup, (5.5-s)+1);
         box1sig->SetFillColor(15);
         box1sig->Draw();
 
         // Dataset name
-        tex->DrawLatex(70, (4.5-s) + 0.35, fileName[s].c_str());
+        tex->DrawLatex(70, (5.5-s) + 0.35, fileName[s].c_str());
     }
 
     // Hardcode the values for plotting. You can also use arrays to do the job, but I'm too lazy here.
-    for (int s = 2; s < 5; s++)
+    for (int s = 2; s < 6; s++)
     {
         switch (s)
         {
-            case 2: // Dilepton
+            case 2: // SS Dilepton
+                limit2sigdown = 0.;
+                limit1sigdown = 7.2;
+                limit1sigup = 17.2;
+                limit2sigup = 0.;
+            case 3: // OS Dilepton
                 limit2sigdown = 10.7;
                 limit1sigdown = 15.5;
                 limit1sigup = 41.4;
                 limit2sigup = 68.5;
                 break;
-            case 3: // Single lepton
+            case 4: // Single lepton
                 limit2sigdown = 7.6;
                 limit1sigdown = 10.7;
                 limit1sigup = 26.2;
                 limit2sigup = 42.8;
                 break;
-            case 4: // Combined
+            case 5: // Combined
                 limit2sigdown = 3.7;
                 limit1sigdown = 8.3;
                 limit1sigup = 21.1;
@@ -123,16 +131,16 @@ void plotXSect()
 
         // Again, 2-sig box
         canvas->cd();
-        box2sig = new TBox(limit2sigdown, 4-s, limit2sigup, (4-s)+1);
+        box2sig = new TBox(limit2sigdown, 5-s, limit2sigup, (5-s)+1);
         box2sig->SetFillColor(17);
         box2sig->Draw();
         // 1-sig box
-        box1sig = new TBox(limit1sigdown, 4-s, limit1sigup, (4-s)+1);
+        box1sig = new TBox(limit1sigdown, 5-s, limit1sigup, (5-s)+1);
         box1sig->SetFillColor(15);
         box1sig->Draw();
 
         // Dataset name
-        tex->DrawLatex(70, (4-s) + 0.35, fileName[s].c_str());
+        tex->DrawLatex(70, (5-s) + 0.35, fileName[s].c_str());
     }
 
     // Separate between our analysis and TOP-16-016
@@ -140,7 +148,7 @@ void plotXSect()
     lineSep->Draw();
 
     // Line indicating where SM is
-    TLine *lineSM = new TLine(1, -0.15, 1, 7);
+    TLine *lineSM = new TLine(1, -0.15, 1, 8);
     lineSM->SetLineColor(kRed);
     lineSM->SetLineWidth((Width_t)5);
     lineSM->Draw();
@@ -165,10 +173,10 @@ void plotXSect()
     expPoints->Draw("SAME P");
 
     // A couple of texts
-    tex->DrawLatex(0,7.1,"#bf{CMS} #it{Work in progress}");
-    tex->DrawLatex(70,7.1,"#scale[0.8]{35.9 fb^{-1} (13 TeV)}");
-    tex->DrawLatex(70,3.15,"#scale[0.8]{2.6 fb^{-1} (13 TeV)}");
-    tex->DrawLatex(2,6.6,"#scale[0.75]{#color[2]{#bf{SM Here}}}");
+    tex->DrawLatex(0,8.1,"#bf{CMS} #it{Work in progress}");
+    tex->DrawLatex(70,8.1,"#scale[0.8]{35.9 fb^{-1} (13 TeV)}");
+    tex->DrawLatex(70,4.15,"#scale[0.8]{2.6 fb^{-1} (13 TeV)}");
+    tex->DrawLatex(2,7.6,"#scale[0.75]{#color[2]{#bf{SM Here}}}");
 
     // Dummy TH1F objects for TLegend
     auto hist1sigDummy = new TH1F("hist1sigDummy","expected #pm 1 #sigma",1,0,1);
